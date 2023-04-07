b1c36096859ac8f1eb6e1552a2ce32726433c408
alomia18 <alomia18@gmail.com>
2023-04-07 01:22:45 -0500

    segundo comit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sin embargo, cuando ingreso ese comando, desde Windows PowerShell, Git
+          Bash y el símbolo del sistema, solo obtengo varias variaciones de
+          "comando no encontrado". En respuesta, desinstalé gh-pages de las
+          dependencias de desarrollo y lo reinstalé como una dependencia normal.
+          Ningún cambio. En este punto, he agotado todas las soluciones que se
+          me ocurren y Google solo ha identificado a las personas que no
+          incluyeron el comando de implementación en package.json.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
